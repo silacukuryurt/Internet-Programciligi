94bb787930907b64d70ad644412dfef6602db069
Sıla Çukuryurt <sila.ck1907@gmail.com>
2024-10-11 11:37:20 +0300

    Add files via upload

---
+++ hafta2.php
@@ -0,0 +1,65 @@
+<?php
+/*$ad="Sıla";
+$soyad="çukuryurt";
+echo $ad." ".$soyad;*/
+
+//Gettype uygulama
+
+/*$degisken=true;
+$degisken2=3;
+$degisken3=5.6;
+//$degisken4="Sıla";
+//$degisken5=array("red","blue","yellow");
+$degisken6=new stdClass;
+$degisken7=NULL;
+$degisken="30";
+echo gettype($degisken)."<br>".
+    gettype($degisken2)."<br>".
+    gettype($degisken3)."<br>".
+    //gettype($degisken4)."<br>".
+    //gettype($degisken5)."<br>".
+    gettype($degisken6)."<br>".
+    gettype($degisken7)."<br>";*/
+
+//Gettype Ekran Görüntüsü
+
+/*$sayi=55;
+echo "<b>Birinci değişkenin adı:
+\$sayi</b>"."<br>".
+"Değeri: ".$sayi."<br>";
+
+$adi="Sıla";
+echo "<b>İkinci değişkenin adı:
+\$adi</b>"."<br>".
+"Türü: ".gettype($adi)."<br>";
+
+$ondalik=5.1234;
+echo "<b>İkinci değişkenin adı:
+\$ondalik</b>"."<br>".
+"Değeri: ".$ondalik."<br>".
+"Türü: ".gettype($ondalik)."<br>";*/
+
+/*$a="32";
+settype($a, "integer");
+var_dump($a);
+echo gettype($a)."<br>";
+
+$b="32";
+settype($b, "string");
+var_dump($b);
+echo gettype($b)."<br>";
+
+$c=true;
+settype($c, "integer");
+var_dump($c);
+echo gettype($c)."<br>";*/
+
+//PHP SABİTLERİ
+7/*echo "<h2>PHP SABİTLERİ</h2>"."<br>".
+    "Kodun Bulunduğu Yer: ".(__LINE__)."<br>".
+    "Dosyanın Bulunduğu Yer: ".(__FILE__)."<br>".
+    "Sunucudaki PHP versiyonu:".phpversion()."<br>".
+"Sunucudaki İşletim Sistemi: ".(PHP_OS)."<br>".
+"Dosyanın Bulunduğu Dizin : ".(__DIR__);*/
+?>
+
